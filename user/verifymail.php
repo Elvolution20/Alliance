09c632884fe30e188465efb8120d33dcb5f53e48
Elvolution20 <elvissmart2020@gmail.com>
2022-08-01 02:33:49 -0700

    commit

--- user/verifymail.php
+++ user/verifymail.php
@@ -16,7 +16,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Alliancecoinmining exchange time| Login </title>
+    <title>Alliancecoinmining | Login </title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
